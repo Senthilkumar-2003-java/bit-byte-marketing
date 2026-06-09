0e52359de030a03dd1ab36d5708f4f1e9eb762a4
Bitbyte=git <reachus@bitbytetech.org>
2026-06-09 15:09:31 +0530

    fix: vite config and redirects

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import fs from 'fs'
 
 export default defineConfig({
   plugins: [
     react(),
-    {
-      name: 'copy-redirects',
-      closeBundle() {
-        fs.writeFileSync(resolve(__dirname, 'dist/_redirects'), '/*    /index.html   200')
-      }
-    }
   ],
   publicDir: 'public',
   build: {
